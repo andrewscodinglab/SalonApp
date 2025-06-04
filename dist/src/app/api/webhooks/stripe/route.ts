ab5f6a199b2dbe7620a1cf37444a092413cfc833
andrewscodinglab <andrewscodinglab@gmail.com>
2025-06-03 22:31:33 -0400

    Update Stripe API version

--- dist/src/app/api/webhooks/stripe/route.ts
+++ dist/src/app/api/webhooks/stripe/route.ts
@@ -8,7 +8,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-05-28.basil',
+  apiVersion: '2024-03-14',
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
